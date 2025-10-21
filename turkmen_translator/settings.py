87255daade619061b9d011e88365ac6847c24c0d
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2025-10-21 11:59:06 +0500

    Fixed settings.py

--- turkmen_translator/settings.py
+++ turkmen_translator/settings.py
@@ -39,7 +39,7 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'translator/static')]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/translator/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
